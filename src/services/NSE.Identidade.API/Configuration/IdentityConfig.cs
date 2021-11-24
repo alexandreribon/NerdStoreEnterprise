517dfb3a642701d4601a054ae8192d350b22fad9
Alexandre Ribon <xribon@gmail.com>
2021-11-24 15:15:25 -0300

    Criando o serviço API Identidade

---
+++ src/services/NSE.Identidade.API/Configuration/IdentityConfig.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSE.Identidade.API.Data;
+using NSE.Identidade.API.Extensions;
+
+namespace NSE.Identidade.API.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfiguration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddDefaultIdentity<IdentityUser>()
+                .AddRoles<IdentityRole>()
+                .AddErrorDescriber<IdentityMensagensPortugues>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            //Config. de Autenticação usando JWT
+            services.AddJwtConfiguration(configuration);
+
+            return services;
+        }
+    }
+}
---
+++ src/services/NSE.Identidade.API/Extensions/AppSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace NSE.Identidade.API.Extensions
+{
+    public class AppSettings
+    {
+        public string Segredo { get; set; }
+        public int ExpiracaoHoras { get; set; }
+        public string Emissor { get; set; }
+        public string ValidoEm { get; set; }
+    }
+}
